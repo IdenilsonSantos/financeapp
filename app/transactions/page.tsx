ed0f614c907910721ba713b1b077e2aa18caeac1
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-11-06 19:30:44 -0300

    feat: ensure transaction table labels are rendered property

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -11,9 +11,9 @@ const Transactions = async () => {
     <div className="p-6 space-y-6">
       <div className="flex w-full justify-between items-center">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full">
-          <ArrowDownUpIcon />
+        <Button className="rounded-full font-bold">
           Adicionar Transação
+          <ArrowDownUpIcon />
         </Button>
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
