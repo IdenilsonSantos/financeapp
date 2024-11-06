15004248554a87c33040ef721596e6b0343d8cba
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-11-06 18:41:21 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,24 @@
-const Transactions = () => {
-    return <h1>transactions page</h1>;
-  };
-  
-  export default Transactions;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+const Transactions = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex w-full justify-between items-center">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          <ArrowDownUpIcon />
+          Adicionar Transação
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default Transactions;
