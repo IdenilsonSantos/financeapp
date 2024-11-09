4fcb273177223baf5170dc9afe816af076ca93f6
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-11-09 12:57:25 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,17 +1,29 @@
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
-import AddTransactionButton from "../_components/ui/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import AddTransactionButton from "../_components/ui/add-transaction-button";
 
-const Transactions = async () => {
-  const transactions = await db.transaction.findMany({});
+const TransactionsPage = async () => {
+  const { userId } = await auth();
 
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
-        <div className="flex w-full justify-between items-center">
+      <div className="space-y-6 p-6">
+        {/* TÍTULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
@@ -21,4 +33,4 @@ const Transactions = async () => {
   );
 };
 
-export default Transactions;
+export default TransactionsPage;
